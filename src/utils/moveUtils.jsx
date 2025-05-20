fa03bcba3f31f4635d787ed08c59e3323e9e1a83
Tanishq Pathak <61777674+reacon@users.noreply.github.com>
2025-05-20 15:03:15 +0530

    Update moveUtils.jsx
    -

--- src/utils/moveUtils.jsx
+++ src/utils/moveUtils.jsx
@@ -1,4 +1,3 @@
-// Move utility functions
 export const toAlgebraic = (row, col) => {
   const files = ["a", "b", "c", "d", "e", "f", "g", "h"];
   const ranks = ["8", "7", "6", "5", "4", "3", "2", "1"];
